30adc9c73c85b0702eb98ba220937dbcd96b9c23
LUJAAA <javier.olivareslr@uanl.edu.mx>
2024-09-25 22:24:42 -0600

    Agregar archivos de proyecto.

---
+++ carbonClon/Controllers/CarbonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace carbonClon.Controllers
+{
+    public class CarbonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
